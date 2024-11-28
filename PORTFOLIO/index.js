81adc5515c2d6fa9a176450e214b673fffe4d837
IPT-BAYBAYON <202201575@cityofmalabonuniversity.edu.ph>
2024-11-28 14:41:04 +0800

    MY PORTFOLIO

---
+++ PORTFOLIO/index.js
@@ -0,0 +1,22 @@
+let navbar = document.querySelector('.navbar');
+let section = document.querySelector('section');
+let navlinks = document.querySelector('header nav a');
+
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scroLLY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(link => {
+                links.classlist.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classlist.add('active');
+            })
+        }
+
+    })
+}
+
